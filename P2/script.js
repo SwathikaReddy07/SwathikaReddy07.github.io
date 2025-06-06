4fa7e037e164e59dcc4d46f704029de4ff46abe3
SwathikaReddy07 <swathikareddytamidela@gmail.com>
2025-06-06 16:42:48 +0530

    updated

--- P2/script.js
+++ P2/script.js
@@ -4,8 +4,9 @@ const showlogin=()=>{
     <h1>Login Form</h1>
     <p><input type="text" id="TxtEmail"></p>
     <p><input type="password" id="TxtPass"></p>
-    <p><button>Log In</button></p>
+    <p><button onclick='showwelcome()'>Log In</button></p>
     <p><button onclick='showregister()'>create Account </p>
+    
     </div>
     
     `
@@ -17,7 +18,17 @@ const showregister=()=>{
     <p><input type="text" id="TxtName"></p>
     <p><input type="text" id="TxtEmail"></p>
     <p><input type="password" id="TxtPass"></p>
-    <button> Register </button>
+    <p><button onclick='showlogin()'> Register </button>
+    <p><button onclick='showlogin()'> Already have an account </button>
+    `
+    root.innerHTML=str
+}
+const showwelcome=()=>{
+    let str=`
+    <div class="App-container">
+    <h1>WELCOME</h1>
+    <p> you are successfully logged in.</p>
+    </div>
     `
     root.innerHTML=str
 }
